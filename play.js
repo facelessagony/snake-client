c4743279ce7d1b24cf44392d9e2331c4953ecd0d
facelessagony <eric.solomon.ca@gmail.com>
2022-10-26 18:03:39 +0000

    initial and play.js

---
+++ play.js
@@ -0,0 +1,29 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "165.227.47.243",// IP address here,
+    port: 50541// PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    // code that does something when the connection is first established
+    console.log('connected!')
+  });
+  
+  conn.on("data", (msg) => {
+    //on incoming data
+    
+    
+    console.log(msg);
+  });
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
