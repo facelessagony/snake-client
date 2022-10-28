b5944d9333258923d40b19ae0eed9785b529c3d6
facelessagony <eric.solomon.ca@gmail.com>
2022-10-28 18:02:23 +0000

    created input.js and moved functions into file

--- play.js
+++ play.js
@@ -1,42 +1,7 @@
-const { stdin } = require('process');
-const client = require('./client');
-
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
-let conn = client.connect();
+connect();
+setupInput();
 
-// setup interface to handle user input from stdin
-
-const setupInput = function () {
-
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  //event handler
-  stdin.on("data", handleUserInput);
-
-  return stdin;
-};
-
-
-
-const handleUserInput = function (key) {
-  // your code here
-  if (key === '\u0003') {
-    process.exit();
-  }
-  
-  if (key === '37' ) {
-    process.stdout.write('Move: left');
-  }
-  if (key === '38' ) {
-    process.stdout.write('Move: up');
-  }
-  if (key === '39' ) {
-    process.stdout.write('Move: right');
-  }
-  if (key === '40' ) {
-    process.stdout.write('Move: left');
-  }
-};
