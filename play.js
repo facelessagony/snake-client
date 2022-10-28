dd714b7ddca462d0df82ddd2764814b77339a024
facelessagony <eric.solomon.ca@gmail.com>
2022-10-28 17:53:39 +0000

    client.js and play.js event handler

--- play.js
+++ play.js
@@ -1,4 +1,42 @@
+const { stdin } = require('process');
 const client = require('./client');
 
+
 console.log("Connecting ...");
-client.connect();
+let conn = client.connect();
+
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  //event handler
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+
+
+const handleUserInput = function (key) {
+  // your code here
+  if (key === '\u0003') {
+    process.exit();
+  }
+  
+  if (key === '37' ) {
+    process.stdout.write('Move: left');
+  }
+  if (key === '38' ) {
+    process.stdout.write('Move: up');
+  }
+  if (key === '39' ) {
+    process.stdout.write('Move: right');
+  }
+  if (key === '40' ) {
+    process.stdout.write('Move: left');
+  }
+};
